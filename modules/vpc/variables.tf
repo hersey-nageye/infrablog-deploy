0823ae972e055fd94cd6a153ce89db3df71f5f32
hersey-nageye <hersey_nageye1@hotmail.com>
2025-05-08 18:38:00 +0100

    moved vpc folder into modules folder

---
+++ modules/vpc/variables.tf
@@ -0,0 +1,30 @@
+variable "vpc_cidrs" {
+  type        = string
+  description = "Cidr block for the VPC"
+}
+
+variable "public_subnet_cidrs" {
+  type        = string
+  description = "value"
+}
+
+variable "private_subnet_cidrs" {
+  type        = list(string)
+  description = "Cidr blocks for the private subnets"
+  default     = ["10.0.2.0/24", "10.0.3.0/24"]
+}
+
+variable "subnet_availability_zones" {
+  type        = list(string)
+  description = "The availability zones for the subnets"
+}
+
+variable "name" {
+  type        = string
+  description = "Name tag used for the resource"
+}
+
+variable "subnet_group_description" {
+  type        = string
+  description = "description of the subnet group"
+}
