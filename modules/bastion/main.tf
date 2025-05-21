706d8cdee486c60a77aad9be778db70fd51fdb2a
hersey-nageye <hersey_nageye1@hotmail.com>
2025-05-21 11:10:46 +0100

    further changes to project

--- modules/bastion/main.tf
+++ modules/bastion/main.tf
@@ -21,6 +21,13 @@ resource "aws_vpc_security_group_ingress_rule" "bastion_ssh" {
   to_port           = 22
 }
 
+# Outbound rule for all traffic on all ports
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
+  security_group_id = aws_security_group.bastion_sg.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1"
+}
+
 # Data block to read existing key pair
 data "aws_key_pair" "existing_key" {
   key_name = "terraform"
--- modules/rds/outputs.tf
+++ modules/rds/outputs.tf
@@ -1,3 +1,7 @@
 output "rds_sg_id" {
   value = aws_security_group.rds_sg.id
 }
+
+output "db_username" {
+  value = random_string.db_username.result
+}
