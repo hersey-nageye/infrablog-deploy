186f787ea045b7296a7a7fab8821414c0d8956ad
hersey-nageye <hersey_nageye1@hotmail.com>
2025-06-27 07:41:23 +0100

    even more debugging

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,7 @@
 
 # outputs.tf in your root Terraform directory
 output "rendered_user_data" {
-  value = templatefile("${path.module}/wordpress-user-data.sh", {
+  value = templatefile("~/Projects/wordpress-project/modules/wordpress/wordpress-user-data.sh", {
     DB_NAME     = var.db_name
     DB_USER     = var.db_user
     DB_PASSWORD = var.db_password
