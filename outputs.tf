623a35d0754b6e94b61b261898d7b26fc67d3e0b
hersey-nageye <hersey_nageye1@hotmail.com>
2025-06-27 07:10:45 +0100

    More debugging

--- outputs.tf
+++ outputs.tf
@@ -1,1 +1,12 @@
 
+output "rendered_user_data" {
+  value = templatefile("${path.module}/wordpress-user-data.sh", {
+    DB_NAME     = var.db_name
+    DB_USER     = var.db_user
+    DB_PASSWORD = var.db_password
+  })
+  description = "The fully rendered user_data script for debugging."
+  # Remove sensitive = true if you want to see the values in local output
+  # You can add it back when you're done debugging.
+  # sensitive   = true
+}
